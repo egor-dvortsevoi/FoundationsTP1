85b2a7db990a2b91be595ac924a4f268ccb62801
Felix <felix.mw.gt@gmail.com>
2026-03-17 07:56:56 +0300

    SCRUM-15: Module-info JavaDoc issue

--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,10 @@
-module HW1 {
+/**
+ * Main module descriptor for the HW1 JavaFX application.
+ */
+module TP1 {
 	requires javafx.controls;
 	requires java.sql;
+	// requires org.junit.jupiter.api; broken right now, but we should add it back for JUnit 5 testing
 	
 	opens applicationMain to javafx.graphics, javafx.fxml;
 }
