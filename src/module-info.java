be2670610b12bdbb93b415fb5512abbfaf4fa627
kalebreshamwalla <184176331+kalebreshamwalla@users.noreply.github.com>
2026-03-22 22:25:54 -0700

    Update module-info.java

--- src/module-info.java
+++ src/module-info.java
@@ -1,7 +1,7 @@
 /**
  * Main module descriptor for the HW1 JavaFX application.
  */
-module FoundationsTP1 {
+module TP1 {
 	requires javafx.controls;
 	requires java.sql;
 	// requires org.junit.jupiter.api; broken right now, but we should add it back for JUnit 5 testing
